bdd8ac959126a3fa4d212b3fdb0a5f0590672972
Derek Vogt <derekvogt3@gmail.com>
2022-08-10 09:33:02 -0400

    commit tree bbb5f5b8b493b3b9fa850ba6b4046a99e6062f8d

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ApplicationRecord
+
+    validates :title, presence: true
+    validates :content, length: {minimum:250}
+    validates :summary, length: {maximum:250}
+    validates :category, inclusion:{in:["Fiction","Non-Fiction"]}
+    validate :custom_validation
+
+    def custom_validation
+        if title == "True Facts"
+            errors.add(:title, "Cant name it as true Facts")
+        end
+    end
+
+
 end
